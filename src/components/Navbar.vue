cdbf01433c36eeb206d7415f9452c282f20a123e
michal1311b <michal1311b@gmail.com>
2021-06-07 22:01:31 +0200

    style serach bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="navbar">
     <img src="@/assets/images/logo.png" />
+    <Searchbar />
   </div>
 </template>
 
 <script>
+import Searchbar from "@/components/Searchbar.vue";
 export default {
   name: "Navbar",
+  components: {
+    Searchbar,
+  },
 };
 </script>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="searchbar">
+    <div class="searchbar__contanier">
+      <div class="searchbar__btn">Search by</div>
+      <select class="searchbar__select">
+        <option value="Name">Name</option>
+        <option value="Identifier">Identifier</option>
+        <option value="Episode">Episode</option>
+      </select>
+      <div class="searchbar__text__container">
+        <input type="text" class="searchbar__text__container__input" />
+        <img
+          src="@/assets/images/search.svg"
+          class="searchbar__text__container__icon"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Searchbar",
+  
+};
+</script>
+
+<style></style>
